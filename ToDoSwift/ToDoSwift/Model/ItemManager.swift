218f5c829b625a1fc4043ff184a870488a14c4df
Aniket Ghode <aniket.ghode@gmail.com>
2017-08-24 09:59:32 +0530

    Added persistence for the ToDo LIst
    Logic to save the todo list to PList
    Serialisation and Deserialisation logic for ToDoItem Struct

--- ToDoSwift/ToDoSwift/Model/ItemManager.swift
+++ ToDoSwift/ToDoSwift/Model/ItemManager.swift
@@ -8,14 +8,50 @@
 
 import Foundation
 
-class ItemManager {
+class ItemManager: NSObject {
     var toDoCount: Int { return toDoItems.count }
     var doneCount: Int { return doneItems.count }
     private var toDoItems: [ToDoItem] = []
     private var doneItems: [ToDoItem] = []
     
+    var toDoPathURL: URL {
+        let fileURLs = FileManager.default.urls(
+            for: .documentDirectory, in: .userDomainMask)
+        
+        guard let documentURL = fileURLs.first else {
+            print("Something went wrong. Documents url could not be found")
+            fatalError()
+        }
+        
+        return documentURL.appendingPathComponent("toDoItems.plist")
+    }
+    
+    override init() {
+        super.init()
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(save),
+            name: .UIApplicationWillResignActive,
+            object: nil)
+        
+        if let nsToDoItems = NSArray(contentsOf: toDoPathURL) {
+            
+            for dict in nsToDoItems {
+                if let toDoItem = ToDoItem(dict: dict as! [String:Any]) {
+                    toDoItems.append(toDoItem)
+                }
+            }
+        }
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+        save()
+    }
+    
     func add(_ item: ToDoItem) {
-        if !toDoItems.contains(where: { $0.title == item.title}) {
+        if !toDoItems.contains(item) {
             toDoItems.append(item)
         }
     }
@@ -42,4 +78,25 @@ class ItemManager {
         toDoItems.removeAll()
         doneItems.removeAll()
     }
+    
+    func save() {
+        let nsToDoItems = toDoItems.map { $0.plistDict }
+        
+        guard nsToDoItems.count > 0 else {
+            try? FileManager.default.removeItem(at: toDoPathURL)
+            return
+        }
+        do {
+            let plistData = try PropertyListSerialization.data(
+                fromPropertyList: nsToDoItems,
+                format: PropertyListSerialization.PropertyListFormat.xml,
+                options: PropertyListSerialization.WriteOptions(0)
+            )
+            try plistData.write(to: toDoPathURL,
+                                options: Data.WritingOptions.atomic)
+        } catch {
+            print(error)
+        }
+    }
+
 }
--- ToDoSwift/ToDoSwift/Model/ToDoItem.swift
+++ ToDoSwift/ToDoSwift/Model/ToDoItem.swift
@@ -8,14 +8,56 @@
 
 import Foundation
 
-struct ToDoItem {
+struct ToDoItem: Equatable {
+    private let titleKey = "titleKey"
+    private let itemDescriptionKey = "itemDescriptionKey"
+    private let timestampKey = "timestampKey"
     let title: String
     let itemDescription: String?
     let timestamp: Double?
     
-    init( title: String, itemDescription: String? = nil, timestamp: Double? = nil) {
+    init(title: String,
+         itemDescription: String? = nil,
+         timestamp: Double? = nil) {
+        
         self.title = title
         self.itemDescription = itemDescription
         self.timestamp = timestamp
     }
+    
+    init?(dict: [String:Any]) {
+        guard let title = dict[titleKey] as? String else
+        { return nil }
+        
+        self.title = title
+        
+        self.itemDescription = dict[itemDescriptionKey] as? String
+        self.timestamp = dict[timestampKey] as? Double
+    }
+    
+    var plistDict: [String:Any] {
+        var dict = [String:Any]()
+        dict[titleKey] = title
+        if let itemDescription = itemDescription {
+            dict[itemDescriptionKey] = itemDescription
+        }
+        if let timestamp = timestamp {
+            dict[timestampKey] = timestamp
+        }
+        return dict
+    }
+    
+}
+
+func ==(lhs: ToDoItem, rhs: ToDoItem) -> Bool {
+    if lhs.timestamp != rhs.timestamp {
+        return false
+    }
+    if lhs.itemDescription != rhs.itemDescription {
+        return false
+    }
+    if lhs.title != rhs.title {
+        return false
+    }
+    return true
 }
